f44a673bc45b19db0a51c6fc52670e72b807eadd
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-06 16:56:38 +0900

    20221206

---
+++ src/ch1206/ThreeDPrinter2.java
@@ -0,0 +1,14 @@
+package ch1206;
+
+public class ThreeDPrinter2 {
+    private Plastic material;
+
+    public void setMaterial(Plastic material) {
+        this.material = material;
+    }
+
+    public Plastic getMaterial() {
+        return material;
+    }
+
+}
