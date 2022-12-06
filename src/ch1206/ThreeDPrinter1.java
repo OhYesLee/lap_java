3536af94b17cd20bc780d4ed3c3e9886a93d8eef
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-06 16:55:45 +0900

    20221206

---
+++ src/ch1206/ThreeDPrinter1.java
@@ -0,0 +1,14 @@
+package ch1206;
+
+public class ThreeDPrinter1 {
+    private Powder material;
+
+    public void setMaterial(Powder material) {
+        this.material = material;
+    }
+
+    public Powder getMaterial() {
+        return material;
+    }
+
+}
