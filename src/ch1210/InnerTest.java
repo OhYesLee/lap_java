ee70a588c8dbe540b8a5ea9e19c0fd2c2b4d6663
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-10 23:33:07 +0900

    20221210

---
+++ src/ch1210/InnerTest.java
@@ -0,0 +1,15 @@
+package ch1210;
+
+public class InnerTest {
+    public static void main(String[] args) {
+        OutClass outClass = new OutClass();
+        System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
+        outClass.usingClass();    // 내부 클래스 기능 호출
+        System.out.println();
+
+        OutClass.InClass inClass = outClass.new InClass();   // 외부 클래스를 이용하여 내부 클래스 생성
+        System.out.println("외부 클래스 변수를 이용하여 내부 클래스 생성");
+        inClass.inTest();
+    }
+
+}
