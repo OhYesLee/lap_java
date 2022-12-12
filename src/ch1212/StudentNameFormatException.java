aa80a4aa4e14b446b969087d5f02116a74ec964f
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-12 22:57:55 +0900

    20221212

---
+++ src/ch1212/StudentNameFormatException.java
@@ -0,0 +1,9 @@
+package ch1212;
+
+public class StudentNameFormatException extends IllegalArgumentException {
+
+    public StudentNameFormatException(String message) {
+        super(message);
+    }
+}
+
