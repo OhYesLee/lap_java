5a53b1cf10e7d005cc7728cc8c8ebc7c5c4dcbc9
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-13 21:21:34 +0900

    표준 입출력 스트림

---
+++ src/ch1213/System.java
@@ -0,0 +1,11 @@
+package ch1213;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class System {
+    public static PrintStream out;
+    public static InputStream in;
+    public static PrintStream err;
+
+}
