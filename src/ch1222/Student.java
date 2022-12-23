106acb3ad3578d156135580fe7dd95914d9d6bb4
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-23 10:10:03 +0900

    레포트 만들기 예제

--- src/ch1222/Student.java
+++ src/ch1222/Student.java
@@ -19,18 +19,18 @@ public class Student {
 
 
     public void setKoreaSubject(String name, int score) {
-        korea.subjectName = name;
-        korea.score = score;
+//        korea.subjectName = name;
+//        korea.score = score;
     }
 
     public void setMathSubject(String name, int score) {
-        math.subjectName = name;
-        math.score = score;
+//        math.subjectName = name;
+//        math.score = score;
     }
 
     public void showStudentSocre() {
-        int total = korea.score + math.score;
-        System.out.println(studentName + " 학생의 총점은 " + total + "점 입니다.");
+//        int total = korea.score + math.score;
+//        System.out.println(studentName + " 학생의 총점은 " + total + "점 입니다.");
 
     }
 
