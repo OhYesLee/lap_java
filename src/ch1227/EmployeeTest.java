15bbdf32f7c85ab849e2be58110b0ba44edfe273
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-27 23:32:10 +0900

    회사원이 입사할 때마다 새로운 사번 부여하기

--- src/ch1227/EmployeeTest.java
+++ src/ch1227/EmployeeTest.java
@@ -4,17 +4,15 @@ import ch1226.Employee;
 
 public class EmployeeTest {
     public static void main(String[] args) {
-        ch1226.Employee employeeLee = new ch1226.Employee();
+        Employee employeeLee = new Employee();
         employeeLee.setEmployeeName("이순신");
-        System.out.println(employeeLee.serialNum);
 
-        ch1226.Employee employeeKim = new Employee();
+        Employee employeeKim = new Employee();
         employeeKim.setEmployeeName("김유신");
-        employeeKim.serialNum++;
 
-        System.out.println(employeeLee.serialNum);
-        System.out.println(employeeKim.serialNum);
-
+        System.out.println(employeeLee.getEmployeeName() + "," + employeeLee.getEmployeeId());
+        System.out.println(employeeKim.getEmployeeName() + "," + employeeKim.getEmployeeId());
     }
+
 
 }
