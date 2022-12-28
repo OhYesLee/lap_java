0cf353de561f8d7b993843d400c5d4a9d396b20b
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-12-28 23:25:52 +0900

    static 메서드 만들기

---
+++ src/ch1228/EmployeeTest2.java
@@ -0,0 +1,10 @@
+package ch1228;
+
+public class EmployeeTest2 {
+    public static void main(String[] args) {
+
+        System.out.println(Employee3.getSerialNum());
+        Employee3.setSerialNum(1003);
+        System.out.println(Employee3.getSerialNum());
+    }
+}
